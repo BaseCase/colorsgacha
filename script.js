e34f5f00d5809910eabf17f3bab88c23d85cf8f2
Casey Brant <casey@caseybrant.com>
2021-04-17 13:54:42 -0500

    basic button styling

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let button;
+
 function init(readyEvent) {
-    console.log("hi there");
+    button = document.querySelector("#the-button");
+    button.addEventListener("mousedown", buttonClicked);
+    document.addEventListener("mouseup", buttonReleased);
+}
+
+function buttonClicked(clickEvent) {
+    button.classList.add('clicked');
+}
+
+function buttonReleased(clickEvent) {
+    button.classList.remove('clicked');
 }
 
 document.addEventListener("DOMContentLoaded", init);
